d9342703a5aa41bc694397175dd55673dcafd674
glitched-sudhanshu <68722462+glitched-sudhanshu@users.noreply.github.com>
2024-04-20 22:57:47 +0530

    added game restart mechanism

--- core/src/main/kotlin/com/example/flappybird/FlappyBird.kt
+++ core/src/main/kotlin/com/example/flappybird/FlappyBird.kt
@@ -22,14 +22,12 @@ class FlappyBird : ApplicationAdapter() {
     private var birdY = 0f
     private var velocity = 0f
     private lateinit var playerHitBox: Circle
-
-//    private lateinit var shapeRenderer: ShapeRenderer
     private var gameState = 0
-    private var gravity = 2f
-    private var topHurdle: Texture? = null
-    private var bottomHurdle: Texture? = null
+    private var gravity = 1f
+    private lateinit var topHurdle: Texture
+    private lateinit var bottomHurdle: Texture
+    private lateinit var gameOverTexture: Texture
     private var hurdleGap = 500f
-    private var maximumOffset = 100f
     private var randomGenerator = java.util.Random(1000)
     private var tubeVelocity = 4f
     private val hurdleX = mutableListOf<Float>()
@@ -49,17 +47,18 @@ class FlappyBird : ApplicationAdapter() {
         bottomHurdle = Texture("bottomtube.png")
         birds.add(Texture("flappybirdup.png"))
         birds.add(Texture("flappybirddown.png"))
+        gameOverTexture = Texture("gameover.png")
         hurdleGap = max(500f, birds[0].height.toFloat() + 20f)
-        birdY = (Gdx.graphics.height / 2 - birds[0].height / 2).toFloat()
         playerHitBox = Circle()
         font = BitmapFont()
         font.setColor(Color.WHITE)
         font.data.scale(12f)
-
-//        shapeRenderer = ShapeRenderer()
-//        maximumOffset = Gdx.graphics.height / 2 - hurdleGap / 2 - 100
         distanceBetweenHurdles = Gdx.graphics.width * 3 / 4f
+        startGame()
+    }
 
+    private fun startGame() {
+        birdY = (Gdx.graphics.height / 2 - birds[0].height / 2).toFloat()
         for (i in 0 until noOfHurdles) {
             tubeOffset.add((randomGenerator.nextFloat() - 0.5f) * (Gdx.graphics.height - hurdleGap - 200))
             hurdleX.add(
@@ -84,7 +83,7 @@ class FlappyBird : ApplicationAdapter() {
             Gdx.graphics.width.toFloat(),
             Gdx.graphics.height.toFloat(),
         )
-        if (gameState != 0) {
+        if (gameState == 1) {
             if (hurdleX[scoringHurdle] < Gdx.graphics.width / 2) {
                 score++
                 if (scoringHurdle < noOfHurdles - 1) {
@@ -94,13 +93,7 @@ class FlappyBird : ApplicationAdapter() {
                 }
             }
             if (Gdx.input.justTouched()) {
-                velocity = -30f
-            }
-            if (birdY > 0 || velocity < 0) {
-                // these two lines will make go bird fall faster when touched
-                velocity += gravity
-                birdY =
-                    (birdY - velocity).coerceAtMost(Gdx.graphics.height.toFloat() - birds[0].height)
+                velocity = -20f
             }
 
             for (i in 0 until noOfHurdles) {
@@ -142,12 +135,38 @@ class FlappyBird : ApplicationAdapter() {
                         topHurdle?.height?.toFloat() ?: 1f,
                     )
             }
-        } else {
+            if (birdY > 0) {
+                // these two lines will make go bird fall faster when touched
+                velocity += gravity
+                birdY =
+                    (birdY - velocity).coerceAtMost(Gdx.graphics.height.toFloat() - birds[0].height)
+            } else {
+                gameState = 2
+            }
+        } else if (gameState == 0) {
             if (Gdx.input.justTouched()) {
                 gameState = 1
             }
+        } else if (gameState == 2) {
+            batch?.draw(
+                gameOverTexture,
+                Gdx.graphics.width / 2f - gameOverTexture.width / 2,
+                Gdx.graphics.height / 2f - gameOverTexture.height / 2,
+            )
+            if (Gdx.input.justTouched()) {
+                gameState = 1
+                hurdleX.clear()
+                tubeOffset.clear()
+                topHurdleBox.clear()
+                bottomHurdleBox.clear()
+                startGame()
+                score = 0
+                hurdleX
+                scoringHurdle = 0
+                velocity = 0f
+            }
         }
-        flapTimer += Gdx.graphics.deltaTime // Update flap timer
+        flapTimer += Gdx.graphics.deltaTime
         if (flapTimer >= flapDelay) {
             flapState = if (flapState == 0) 1 else 0
             flapTimer = 0f
@@ -158,31 +177,18 @@ class FlappyBird : ApplicationAdapter() {
             ((Gdx.graphics.width / 2) - (birds[flapState].width.div(2))).toFloat(),
             birdY,
         )
-//        shapeRenderer.begin(ShapeRenderer.ShapeType.Filled)
         playerHitBox.set(Gdx.graphics.width / 2f, birdY + birds[0].height / 2, birds[0].width / 2f)
         font.draw(batch, score.toString(), 100f, 200f)
-//        shapeRenderer.circle(playerHitBox.x, playerHitBox.y, playerHitBox.radius)
         for (i in 0 until noOfHurdles) {
-//            shapeRenderer.rect(
-//                hurdleX[i],
-//                Gdx.graphics.height / 2 + hurdleGap / 2 + tubeOffset[i],
-//                topHurdle?.width?.toFloat() ?: 1f,
-//                topHurdle?.height?.toFloat() ?: 1f,
-//            )
-//            shapeRenderer.rect(
-//                hurdleX[i],
-//                Gdx.graphics.height / 2 - hurdleGap / 2 - (
-//                    bottomHurdle?.height
-//                        ?: 1
-//                ) + tubeOffset[i],
-//                topHurdle?.width?.toFloat() ?: 1f,
-//                topHurdle?.height?.toFloat() ?: 1f,
-//            )
-            if (Intersector.overlaps(playerHitBox, topHurdleBox[i]) || Intersector.overlaps(playerHitBox, bottomHurdleBox[i])) {
-                gameState = 0
+            if (Intersector.overlaps(playerHitBox, topHurdleBox[i]) ||
+                Intersector.overlaps(
+                    playerHitBox,
+                    bottomHurdleBox[i],
+                )
+            ) {
+                gameState = 2
             }
         }
-//        shapeRenderer.end()
         batch?.end()
     }
 }
