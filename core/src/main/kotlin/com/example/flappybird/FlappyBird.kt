1d7c3cf1b3289fb87b24a82fa6921bd4e287013c
glitched-sudhanshu <68722462+glitched-sudhanshu@users.noreply.github.com>
2024-04-20 21:04:19 +0530

    added pipes

--- core/src/main/kotlin/com/example/flappybird/FlappyBird.kt
+++ core/src/main/kotlin/com/example/flappybird/FlappyBird.kt
@@ -4,6 +4,7 @@ import com.badlogic.gdx.ApplicationAdapter
 import com.badlogic.gdx.Gdx
 import com.badlogic.gdx.graphics.Texture
 import com.badlogic.gdx.graphics.g2d.SpriteBatch
+import java.lang.Float.max
 
 class FlappyBird : ApplicationAdapter() {
     // cant instantiate here, coz we cant call it's constructor before create()
@@ -17,16 +18,50 @@ class FlappyBird : ApplicationAdapter() {
     private var velocity = 0f
     private var gameState = 0
     private var gravity = 2f
+    private var topHurdle: Texture? = null
+    private var bottomHurdle: Texture? = null
+    private var hurdleGap = 500f
+    private var maximumOffset = 100f
+    private var randomGenerator = java.util.Random(1000)
+    private var tubeVelocity = 4f
+    private var hurdleX = mutableListOf<Float>()
+    private var tubeOffset = mutableListOf<Float>()
+    private var noOfHurdles = 4
+    private var distanceBetweenHurdles = 100f
 
     override fun create() {
         batch = SpriteBatch()
         background = Texture("background.png")
+        topHurdle = Texture("toptube.png")
+        bottomHurdle = Texture("bottomtube.png")
         birds.add(Texture("flappybirdup.png"))
         birds.add(Texture("flappybirddown.png"))
+        hurdleGap = max(500f, birds[0].height.toFloat() + 20f)
         birdY = (Gdx.graphics.height / 2 - birds[0].height / 2).toFloat()
+//        maximumOffset = Gdx.graphics.height / 2 - hurdleGap / 2 - 100
+        distanceBetweenHurdles = Gdx.graphics.width * 3 / 4f
+        for (i in 0 until noOfHurdles) {
+            tubeOffset.add((randomGenerator.nextFloat() - 0.5f) * (Gdx.graphics.height - hurdleGap - 200))
+            hurdleX.add(
+                (
+                    Gdx.graphics.width / 2 - (
+                        topHurdle?.width
+                            ?: 1
+                    ) / 2
+                ).toFloat() + Gdx.graphics.width + i * distanceBetweenHurdles,
+            )
+        }
     }
 
     override fun render() {
+        batch?.begin()
+        batch?.draw(
+            background,
+            0f,
+            0f,
+            Gdx.graphics.width.toFloat(),
+            Gdx.graphics.height.toFloat(),
+        )
         if (gameState != 0) {
             if (Gdx.input.justTouched()) {
                 velocity = -30f
@@ -36,6 +71,29 @@ class FlappyBird : ApplicationAdapter() {
                 velocity += gravity
                 birdY =
                     (birdY - velocity).coerceAtMost(Gdx.graphics.height.toFloat() - birds[0].height)
+            }
+
+            for (i in 0 until noOfHurdles) {
+                if (hurdleX[i] < -(topHurdle?.width ?: 1)) {
+                    hurdleX[i] += noOfHurdles * distanceBetweenHurdles
+                    tubeOffset[i] =
+                        (randomGenerator.nextFloat() - 0.5f) * (Gdx.graphics.height - hurdleGap - 200)
+                } else {
+                    hurdleX[i] = hurdleX[i] - tubeVelocity
+                }
+                batch?.draw(
+                    topHurdle,
+                    hurdleX[i],
+                    Gdx.graphics.height / 2 + hurdleGap / 2 + tubeOffset[i],
+                )
+                batch?.draw(
+                    bottomHurdle,
+                    hurdleX[i],
+                    Gdx.graphics.height / 2 - hurdleGap / 2 - (
+                        bottomHurdle?.height
+                            ?: 1
+                    ) + tubeOffset[i],
+                )
             }
         } else {
             if (Gdx.input.justTouched()) {
@@ -48,14 +106,6 @@ class FlappyBird : ApplicationAdapter() {
             flapTimer = 0f
         }
 
-        batch?.begin()
-        batch?.draw(
-            background,
-            0f,
-            0f,
-            Gdx.graphics.width.toFloat(),
-            Gdx.graphics.height.toFloat(),
-        )
         batch?.draw(
             birds[flapState],
             ((Gdx.graphics.width / 2) - (birds[flapState].width.div(2))).toFloat(),
